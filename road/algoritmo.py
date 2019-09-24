f33b4972fb1d242bf6fa1f379772f770c09e953e
xanderMMR <40546745+xanderMMR@users.noreply.github.com>
2019-09-24 17:45:39 -0500

    Ordenamiento de datos y lectura de datos

--- road/algoritmo.py
+++ road/algoritmo.py
@@ -1,51 +1,99 @@
-def phspprg(width, height, rectangles, sorting="width"):
-  
-    if sorting not in ["width", "height" ]:
-        raise ValueError("The algorithm only supports sorting by width or height but {} was given.".format(sorting))
-    if sorting == "width":
-        wh = 0
-    else:
-        wh = 1
-    logger.debug('The original array: {}'.format(rectangles))
-    result = [None] * len(rectangles) # resulta = [None]
-    remaining = deepcopy(rectangles) #Lo que hace es tener en una lista auxiliar, referenciado a los valores iniciales
-    for idx, r in enumerate(remaining): #Enumerate, bota un contador y el valor --> (0, rectangle[0])(1, rectangle[1])
-        if r[0] > r[1]:
-            remaining[idx][0], remaining[idx][1] = remaining[idx][1], remaining[idx][0] #Lo giro 90 grados
-    #logger.debug('Swapped some widths and height with the following result: {}'.format(remaining))
-    
-    sorted_indices = sorted(range(len(remaining)), key=lambda x: -remaining[x][wh]) # sorted_indices = [1, 0]
-    '''
-    La función sorted puede tener un parametro llamado "key", este manda a la hora de ordenar, 
-    si key = len, le decimos que ordene las ids ---> range(n), pero según quien es mayor ---> remaining[x],
-    botaría números de 0 a n, ordenados de acuerdo a su peso
-    en este caso le decimos que ordene según " - remaining[x][wh] "
-    o sea, que de remaining, agarre el primer valor de cada valor, que viene a ser la coordenada " x ",
-    pero que lo ponga negativo, o sea, que sea decreciente o reverso, esto podría cambiarse por
-    el tercer parámetro que tiene sorted, sería sorted(iterable, key, reverse), sería poner reverse = True
-    y tendría el mismo resultado, o eso creo :p
-    '''
-    #logger.debug('The sorted indices: {}'.format(sorted_indices))
-    sorted_rect = [remaining[idx] for idx in sorted_indices] #Acá, según el orden que envía sorted_indices, este agarra  
-    #el mismo orden, pero, agarra ya las coordenadas, teniendo un orden a la hora de tratar de ordenar los rectángulos
-    #logger.debug('The sorted array is: {}'.format(sorted_rect))
-    x, y, w, h, H = 0, 0, 0, 0, 0
-    while sorted_indices: #cola  [1, 0]
-        idx = sorted_indices.pop(0) #sprted queda con [0] porque le quitas el 1 idx = 1
-        r = remaining[idx]  # remain[1] = [10,20] 
-        #En el if
-        #Acá le está diciendo que, si es que el rectángulo ingresado es mayor a la placa, que lo dibuje igual, y que avise
-        #cuando queda de ancho (w), y cuanto es la nueva altura(h), también avisa en donde empieza el siguiente rect (x, y)
-        if r[1] > width or r[1] > height:#si la altura es mayor al ancho
-            result[idx] = Rectangle(x, y, r[0], r[1])
-            x, y, w, h, H = r[0], H, width - r[0], r[1], H + r[1] 
-        else: #si la altura es menor al ancho, podemos rotarlo
-            result[idx] = Rectangle(x, y, r[1], r[0]) #lo rotamos
-            x, y, w, h, H = r[1], H, width - r[1], r[0], H + r[0]#cuando queda de ancho (w), y cuanto es la nueva altura(h),
-                                                                 #también avisa en donde empieza el siguiente rect (x, y)
-     #       print(x, y, w, h, H)                                      
-        recursive_packing(x, y, w, h, 1, remaining, sorted_indices, result)
-        x, y = 0, H
-    #logger.debug('The resulting rectangles are: {}'.format(result))
+from collections import namedtuple
+import os
 
-    return H, result
+os.system("cls")
+I=[]
+
+with open("texto1.txt", "r") as archivo:
+    lines = archivo.readlines()
+    start=2
+    end=len(lines)-2
+    ID=[None]*end
+    anc=[int]*end
+    alt=[int]*end
+    for start in range(end):
+        m =lines[0].split()
+       
+        n= lines[1].split()
+        x= lines[start+2].split()
+
+        ID[start]=x[0]
+        anc[start]=int(x[1])
+        alt[start]=int(x[2])
+ancho=int(m[0])  
+alto=int(m[1])
+cantidad=int(n[0])
+anchoCajas=anc
+altoCajas=alt
+for start in range(end):
+    areaCajas=anchoCajas[start]*altoCajas[start]
+    sumatoriaAreaCajas=0
+    sumatoriaAreaCajas+=areaCajas
+    print (sumatoriaAreaCajas)
+
+
+def datos():
+    listaDatos=[None]*end
+    for start in range(end):
+        listaDatos[start]=[anc[start],alt[start]]
+    return listaDatos
+
+def verDatos():
+    print("AnchoPlancha:"+str(ancho),"AltoPlancha:"+str(alto))
+    for start in range(end):
+        print("ID("+ID[start]+")","\tancho:"+ str(anc[start]),"\talto:"+str(alt[start]))
+
+verDatos()
+from collections import namedtuple
+import sys
+ide='ID'
+caja = namedtuple('ID', [ide,'x', 'y', 'w', 'h'])
+
+def ordenarCajas(ancho, alto, cajas): 
+    n = [None] * cantidad 
+    aux = cajas
+    print (aux)
+    for idx, r in enumerate(aux):
+        if r[0] > r[1]:
+            aux[idx][0], aux[idx][1] = aux[idx][1], aux[idx][0]
+    ordenarIndices = sorted(range(len(aux)), key=lambda m: -aux[m][0])
+    print(aux)
+    print (ordenarIndices)
+    x, y, w, h, H = 0, 0, 0, 0, 0
+    while ordenarIndices:
+        idx = ordenarIndices.pop(0)
+        print(idx)
+        dato = aux[idx]
+        print (dato)
+
+        print (")))))",dato[1])
+        
+        if dato[1] > ancho or dato[1] > alto:
+            n[idx] = caja(ID[idx], x, y, dato[0], dato[1])
+            print ("*****",n[idx])
+            x, y, w, h, H = dato[0], H, ancho - dato[0], dato[1], H + dato[1] 
+
+        else: 
+            n[idx] = caja(ID[idx],x, y, dato[1], dato[0]) 
+            x, y, w, h, H = dato[1], H, ancho - dato[1], dato[0], H + dato[0]      
+        backtracking(x, y, w, h, 1, aux, ordenarIndices, n)
+        x,y=0,H
+    return n
+
+def backtracking(posX, posY, ancho, alto, D, R, indices, resultado):
+    
+    prioridad = 6
+    for idx in indices:  
+        for j in range(0, D + 1): 
+          
+            if prioridad > 1 and R[idx][(0 + j) % 2] == ancho and R[idx][(1 + j) % 2] == alto:
+                prioridad, orientacion, mejor = 1, j, idx              
+                break
+            elif prioridad > 2 and R[idx][(0 + j) % 2] == ancho and R[idx][(1 + j) % 2] < alto:
+                prioridad, orientacion, mejor = 2, j, idx 
+            elif prioridad > 3 and R[idx][(0 + j) % 2] < ancho and R[idx][(1 + j) % 2] == alto:
+                prioridad, orientacion, mejor = 3, j, idx 
+            elif prioridad > 4 and R[idx][(0 + j) % 2] < ancho and R[idx][(1 + j) % 2] < alto:
+                prioridad, orientacion, mejor = 4, j, idx
+            elif prioridad > 5: 
+                prioridad, orientacion, mejor = 5, j, idx
